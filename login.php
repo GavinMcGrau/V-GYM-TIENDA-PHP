d808b68cde3bc53a6acf6547a980b06cbc40ecb1
GavinMcGrau <64991581+GavinMcGrau@users.noreply.github.com>
2021-11-07 21:24:38 +0100

    3

--- login.php
+++ login.php
@@ -61,7 +61,7 @@ if($array ['contar']>0 ){?>
 
 
     
-<div class="d-flex flex-column vh-100 flex-shrink-0 p-3 text-white bg-dark" style="width: 250px;"> <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"> <svg class="bi me-2" width="40" height="32"> </svg> <span class="fs-4">BBBootstrap</span> </a>
+<div class="d-flex flex-column vh-100 flex-shrink-0 p-3 text-white bg-dark" style="width: 250px;"> <a href="/" class="d-flex align-items-center mb-5 mb-md-5 me-md-auto text-white text-decoration-none"> <svg class="bi me-2" width="40" height="32"> </svg> <h2 class="fs-4">BreakEnds</h2> </a>
     <hr>
     <ul class="nav nav-pills flex-column mb-auto">
         <li class="nav-item"> <a href="#" class="nav-link active" aria-current="page"> <i class="fa fa-home"></i><span class="ms-2">Home</span> </a> </li>
@@ -70,12 +70,11 @@ if($array ['contar']>0 ){?>
         <li> <a href="#" class="nav-link text-white"> <i class="fa fa-cog"></i><span class="ms-2">Settings</span> </a> </li>
         <li> <a href="#" class="nav-link text-white"> <i class="fa fa-bookmark"></i><span class="ms-2">Bookmarks</span> </a> </li>
     </ul>
-        <h3 class="d-flex align-items-center text-white text-decoration-none " aria-expanded="false"> <?php echo $nombreUsuario; ?></h3>
+        <h3 class="d-flex align-items-center text-white text-decoration-none nombre" aria-expanded="false"> <?php echo $nombreUsuario; ?></h3>
 
     <hr>
 
-    <div class="dropdown">  <img src="<?php echo $fotoUsuario; ?>" alt="" width="200" height="200" class="rounded-circle me-2">  </a>
-              </ul>
+    <div class="dropdown">  <img src="<?php echo $fotoUsuario; ?>" alt="" class="rounded-circle  fotoUser" >  
     </div>
 </div>
 
