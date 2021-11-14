60d7738b6cafbaa5390b8b29eeed87f987be8bb3
GavinMcGrau <64991581+GavinMcGrau@users.noreply.github.com>
2021-11-15 00:05:12 +0100

    +ejercicios

--- masActividades.php
+++ masActividades.php
@@ -2,28 +2,44 @@
 
 session_start();
 include "connection.php";
+
+
 include "plantillaHtml.php";
 
+?>
+    <div class="boton" style="margin-left: 27vw;">Todo</div>
+     <div class="boton" style="margin-left: 45vw;">Tren Superior</div>
+      <div class="boton" style="margin-left: 63vw;">Tren Inferior</div>
+       
 
-
- $sqlImagenActividad="SELECT foto as imagenActividad,clase as claseActividad,subclase as subclaseActividad,nombre as nombreActividad from actividades";
+ <div class="container contenedorCentral">
+  
+            <div class="container contenedorActividades">
+    <div class="card-columns ">
+        <?php
+ $sqlImagenActividad="SELECT foto as imagenActividad,clase as claseActividad,subclase as subclaseActividad,nombre as nombreActividad from actividades where idUsuario=1";
           $consultaImagenActividad=mysqli_query($con,$sqlImagenActividad);
-          $knowActividad = mysqli_fetch_array($consultaImagenActividad);
+      
+          while (($knowActividad = mysqli_fetch_array($consultaImagenActividad))!=NULL){
+        
           $imagenActividad = $knowActividad["imagenActividad"];
           $claseActividad = $knowActividad["claseActividad"];
           $subClaseActividad = $knowActividad["subclaseActividad"];
           $nombreActividad =$knowActividad["nombreActividad"];
-          while (($fila = mysqli_fetch_array($consultaImagenActividad))!=NULL){
-    
             ?> 
-            <div class="card">
+
+           
+            <div class="card actividadPrincipal">
+               
         <a href="#">
 
-        <img class="card-img-top" src= "<?php echo $imagenActividad?>" alt="Card image cap">
+        <img class="card-img-top " src= "<?php echo $imagenActividad?>" alt="Card image cap">
         <div class="card-body">
          
-          <h5 class="card-title"><?php echo ucwords($nombreActividad) ?></h5>
+          <h5 class="card-title "><?php echo ucwords($nombreActividad) ?></h5>
           <p class="card-text"><?php echo  $claseActividad ?> - <?php echo $subClaseActividad?> </p>
       </div>
         </a>
           </div>
+          
+<?php }?>
--- plantillaHtml.php
+++ plantillaHtml.php
@@ -64,7 +64,7 @@ $fotoUsuario = $knowFoto['imagens'];
             </li>
             <li>
                 <a href="configUsuario.php" class="nav-link text-white"> <span class="ms-2">Mi prefil</span> </a>
-            </li>
+                </li>
             <li>
                 <a href="#" class="nav-link text-white"> <span class="ms-2">Settings</span> </a>
             </li>
@@ -88,6 +88,8 @@ $fotoUsuario = $knowFoto['imagens'];
         </div>
   </div>
 </div>
+
+
  
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
