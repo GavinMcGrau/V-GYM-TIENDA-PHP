2242927c216494b60ee35c2af248fe29d73dac37
GavinMcGrau <64991581+GavinMcGrau@users.noreply.github.com>
2021-11-16 19:09:09 +0100

    masActividades

--- masActividades.php
+++ masActividades.php
@@ -13,10 +13,17 @@ include "plantillaHtml.php";
        
 
  <div class="container contenedorCentral">
-  
+   <?php 
+        
+        function anyadirActividad($nombreActividad,$claseActividad,$subClaseActividad,$descripcionActividad,$imagenActividad,$idUsuario,$series,$repeticiones,$dia){
+            $sqlAnyadir ="INSERT INTO actividades (`id`, `nombre`, `foto`, `clase`, `subclase`, `descripcion`, `hecha`, `idUsuario`, `cantidad`, `repeticiones`, `dia`) VALUES ('','$nombreActividad','$imagenActividad','$claseActividad','$subClaseActividad','$descripcionActividad','','$idUsuario','$series','$repeticiones','$dia')";
+            $consultaAnyadir=mysqli_query($con,$sqlAnyadir);
+        }
+        
+        ?>
             
         <?php
- $sqlImagenActividad="SELECT foto as imagenActividad,clase as claseActividad,subclase as subclaseActividad,nombre as nombreActividad from actividades where idUsuario=1";
+ $sqlImagenActividad="SELECT foto as imagenActividad,descripcion as descripcionActividad,clase as claseActividad,subclase as subclaseActividad,nombre as nombreActividad from actividades where idUsuario=1";
           $consultaImagenActividad=mysqli_query($con,$sqlImagenActividad);
       
           while (($knowActividad = mysqli_fetch_array($consultaImagenActividad))!=NULL){
@@ -25,6 +32,7 @@ include "plantillaHtml.php";
           $claseActividad = $knowActividad["claseActividad"];
           $subClaseActividad = $knowActividad["subclaseActividad"];
           $nombreActividad =$knowActividad["nombreActividad"];
+          $descripcionActividad= $knowActividad["descripcionActividad"];
 
             ?> 
 
@@ -42,25 +50,28 @@ include "plantillaHtml.php";
                 </div>
                 <img src="<?php echo $imagenActividad?>" alt="imagen"><!-- Imagen frontal -->
             </div>
-
-            
             <div class="flip-card-back"><!-- reverso de la tarjeta -->
                 <div class="row no-gutters">
                     <div class="card-body">
-                        <h5 class="card-title"><?php echo ucwords($nombreActividad) ?></h5>
-                        <p class="card-text pl-2 mt-2"><?php echo  $claseActividad ?> - <?php echo $subClaseActividad?> </p>
+                        <iframe class="videosYoutube" src="<?php echo $descripcionActividad ;?>"></iframe>
+
                     </div>
                 </div>
             </div>
             
         </div>
         
+       
+
     </div>
     <div class="hola" >
         <form method='post'  class='login' id='login' action='./login.php'>
-
-<input name='cantidad' class=' ' placeholder='0' type='number' required autofocus>
-
+<h5 style="color:white;">Series</h5>
+<input name='Series' class='inputs' style="width:4vw;" placeholder='Series' type='number'  required autofocus>
+<br>
+<h5 style="color:white;">Repeticiones</h5>
+<input name='Repeticiones' class=' ' style="width:4vw;"  placeholder='Repeticiones' type='number' required autofocus>
+<br>
 <div class="box">
 <select>
 
@@ -73,6 +84,8 @@ include "plantillaHtml.php";
     <option>domingo</option>
 
 </select> 
+<br>
+<br>
  </div>
      <button class='enviarLogin' name='submit ' type='submit ' id='contact-submit ' data-submit='...Enviant '>Añadir</button>
  
