3a882a3ca8aed74adea102b9a1d2fe080fea3df3
GavinMcGrau <64991581+GavinMcGrau@users.noreply.github.com>
2021-11-21 23:02:53 +0100

    sqlExport

--- 1sqlcreator.sql
+++ 1sqlcreator.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Temps de generació: 21-11-2021 a les 21:50:08
--- Versió del servidor: 10.4.21-MariaDB
--- Versió de PHP: 8.0.12
+-- Tiempo de generación: 21-11-2021 a las 23:01:57
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 8.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,15 +18,15 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de dades: `gym`
+-- Base de datos: `gym`
 --
-CREATE DATABASE IF NOT EXISTS `gym` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+CREATE DATABASE IF NOT EXISTS `gym` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish2_ci;
 USE `gym`;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de la taula `actividades`
+-- Estructura de tabla para la tabla `actividades`
 --
 
 CREATE TABLE `actividades` (
@@ -35,16 +35,16 @@ CREATE TABLE `actividades` (
   `foto` varchar(500) CHARACTER SET latin1 NOT NULL,
   `clase` varchar(30) CHARACTER SET latin1 NOT NULL,
   `subclase` varchar(30) CHARACTER SET latin1 DEFAULT NULL,
-  `descripcion` varchar(500) COLLATE utf8mb4_spanish_ci NOT NULL,
-  `hecha` varchar(3) COLLATE utf8mb4_spanish_ci NOT NULL DEFAULT 'no',
-  `idUsuario` varchar(30) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `descripcion` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
+  `hecha` varchar(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL DEFAULT 'no',
+  `idUsuario` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
   `cantidad` int(2) NOT NULL,
   `repeticiones` int(2) NOT NULL,
   `dia` varchar(18) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
--- Bolcament de dades per a la taula `actividades`
+-- Volcado de datos para la tabla `actividades`
 --
 
 INSERT INTO `actividades` (`id`, `nombre`, `foto`, `clase`, `subclase`, `descripcion`, `hecha`, `idUsuario`, `cantidad`, `repeticiones`, `dia`) VALUES
@@ -59,27 +59,28 @@ INSERT INTO `actividades` (`id`, `nombre`, `foto`, `clase`, `subclase`, `descrip
 (9, 'Plancha Lateral', 'https://www.operaciontransformer.com/wp-content/uploads/2019/05/WBPR-quoblog-271213_09643.jpg', 'Tren Superior', 'Abdominales Laterales', 'https://www.youtube.com/embed/ewr7YpHJOv8', '0', '1', 0, 0, ''),
 (11, 'Fondos de triceps', 'https://as01.epimg.net/deporteyvida/imagenes/2017/08/06/portada/1502014266_073304_1502014358_noticia_normal.jpg\r\n', 'Tren Superior', 'Triceps', 'https://www.youtube.com/embed/PYapgguXgT8', 'off', 'cacscasca@gmal.com', 12, 32, 'jueves'),
 (13, 'flexiones', 'https://i.blogs.es/8f0e76/crossfit/450_1000.jpg', 'Tren Superior', 'Brazos', 'https://www.youtube.com/embed/NklcXdowKww', 'on', 'cacscasca@gmal.com', 3, 10, 'jueves'),
-(33, 'Fondos de triceps', 'https://as01.epimg.net/deporteyvida/imagenes/2017/08/06/portada/1502014266_073304_1502014358_noticia_normal.jpg\r\n', 'Tren Superior', 'Triceps', 'https://www.youtube.com/embed/PYapgguXgT8', 'no', 'cacscasca@gmal.com', 1, 12, 'viernes');
+(33, 'Fondos de triceps', 'https://as01.epimg.net/deporteyvida/imagenes/2017/08/06/portada/1502014266_073304_1502014358_noticia_normal.jpg\r\n', 'Tren Superior', 'Triceps', 'https://www.youtube.com/embed/PYapgguXgT8', 'no', 'cacscasca@gmal.com', 1, 12, 'viernes'),
+(35, 'Fondos de triceps', 'https://as01.epimg.net/deporteyvida/imagenes/2017/08/06/portada/1502014266_073304_1502014358_noticia_normal.jpg\r\n', 'Tren Superior', 'Triceps', 'https://www.youtube.com/embed/PYapgguXgT8', 'no', '', 2, 23, 'miercoles');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de la taula `usuario`
+-- Estructura de tabla para la tabla `usuario`
 --
 
 CREATE TABLE `usuario` (
-  `nombre` varchar(20) COLLATE utf8mb4_spanish_ci NOT NULL,
-  `contrasenya` varchar(20) COLLATE utf8mb4_spanish_ci NOT NULL,
-  `correo` varchar(30) COLLATE utf8mb4_spanish_ci NOT NULL,
-  `rol` varchar(20) COLLATE utf8mb4_spanish_ci NOT NULL DEFAULT 'users',
-  `imagen` varchar(500) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `nombre` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
+  `contrasenya` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
+  `correo` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
+  `rol` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL DEFAULT 'users',
+  `imagen` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci DEFAULT NULL,
   `edad` int(3) NOT NULL,
   `peso` float NOT NULL,
   `altura` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
--- Bolcament de dades per a la taula `usuario`
+-- Volcado de datos para la tabla `usuario`
 --
 
 INSERT INTO `usuario` (`nombre`, `contrasenya`, `correo`, `rol`, `imagen`, `edad`, `peso`, `altura`) VALUES
@@ -89,30 +90,30 @@ INSERT INTO `usuario` (`nombre`, `contrasenya`, `correo`, `rol`, `imagen`, `edad
 ('Usuario', '1234', 'usuario@drop.com', 'users', NULL, 0, 0, 0);
 
 --
--- Índexs per a les taules bolcades
+-- Índices para tablas volcadas
 --
 
 --
--- Índexs per a la taula `actividades`
+-- Indices de la tabla `actividades`
 --
 ALTER TABLE `actividades`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índexs per a la taula `usuario`
+-- Indices de la tabla `usuario`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`correo`);
 
 --
--- AUTO_INCREMENT per les taules bolcades
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT per la taula `actividades`
+-- AUTO_INCREMENT de la tabla `actividades`
 --
 ALTER TABLE `actividades`
-  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
