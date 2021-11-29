dcf6a0757fbe1a8bae5db53ee041f11316e44f14
GavinMcGrau <64991581+GavinMcGrau@users.noreply.github.com>
2021-11-29 16:42:53 +0100

    a

--- tabla.php
+++ tabla.php
@@ -481,12 +481,4 @@
        
     </table>
     
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="//cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
-    <script>
-        $(document).ready( function () {
-            $('table.display').DataTable();
-        } );
-    </script>
-</body>
-</html>
+   
